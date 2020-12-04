fdd26bc2498145b054dd1572bd6b743b593f426e
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-12-04 11:37:56 +0100

    hadnleOnSubmit and search filed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
+import Loading from "./Loading";
 require("dotenv").config(); // ? <== require the dotenv to use .env
 const getApiToken = require("./components/urltoken"); // ! <== request urltoken from a .env file
-
-const BLACKAPI = getApiToken.urltoken.BLACKAPI; // ? <== the data url 
+const BLACKAPI = getApiToken.urltoken.BLACKAPI; // ? <== the data url
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     const proxyurl = getApiToken.urltoken.PROXYURL; // ? <== This API enables cross-origin requests to anywhere.
     fetch(proxyurl + BLACKAPI) // ! here i did this bc i need to enable the cross-origin request
       .then((res) => res.json())
       .then((data) => {
-        console.log(data); // ** print the response data json
-        setMovies(data);
+        setMovies(data.data);
+        setLoading(false);
       });
   }, []);
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+  };
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
   return (
-    <div className="app">
-      {movies.length > 0 && movies.map((movie) => <Movie />)}
-    </div>
+    <>
+      <header>
+        <h3 className="b3ns44d">B3ns44d</h3>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="search"
+            type="search"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </header>
+      <div className="movie-container">
+        {!loading ? (
+          movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie._id} {...movie} />)
+        ) : (
+          <Loading />
+        )}
+      </div>
+    </>
   );
 }
 
