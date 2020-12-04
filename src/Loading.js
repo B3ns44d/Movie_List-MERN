d5e77b963c1f37943d470b9f9da090e8b67c0e92
Abdessamad <abdsamad23bnsaad@gmail.com>
2020-12-04 11:38:16 +0100

    create the loading message

---
+++ src/Loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loading.css";
+
+function Loading() {
+  return (
+    <div className="loading">
+      <h1>Getting Data....</h1>
+    </div>
+  );
+}
+export default Loading;
